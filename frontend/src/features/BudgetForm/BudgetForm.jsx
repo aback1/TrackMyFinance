a0ee06960d0aa9beb062d029b8ecc2193c338a08
ABack <flinix@gmail.com>
2025-02-02 19:43:34 +0100

    created DockerImage ,fixed Cost calculation updated composer.json

--- frontend/src/features/BudgetForm/BudgetForm.jsx
+++ frontend/src/features/BudgetForm/BudgetForm.jsx
@@ -2,31 +2,70 @@ import '../../index.css';
 import Button from '../../components/Button.jsx';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Icon from '../../components/Icon.jsx';
 
 export default function BudgetForm() {
   const [month, setMonth] = useState(new Date());
-  const [income, setIncome] = useState('');
-  const [rent, setRent] = useState('');
-  const [sideCosts, setSideCosts] = useState('');
-  const [foodAndDrinksCosts, setFoodAndDrinksCosts] = useState('');
-  const [mobilityCosts, setMobilityCosts] = useState('');
-  const [hobbyCosts, setHobbyCosts] = useState('');
-  const [insuranceCosts, setInsuranceCosts] = useState('');
-  const [monthlySavingsCosts, setMonthlySavingsCosts] = useState('');
+  const [income, setIncome] = useState(0);
+  const [rent, setRent] = useState(0);
+  const [sideCosts, setSideCosts] = useState(0);
+  const [foodAndDrinksCosts, setFoodAndDrinksCosts] = useState(0);
+  const [mobilityCosts, setMobilityCosts] = useState(0);
+  const [hobbyCosts, setHobbyCosts] = useState(0);
+  const [insuranceCosts, setInsuranceCosts] = useState(0);
+  const [monthlySavingsCosts, setMonthlySavingsCosts] = useState(0);
+  const [saldo, setSaldo] = useState(0);
 
   const textArr = [
-    'TEST',
-    'Test2',
-    'Test3',
-    'sdsdad',
-    'dsadada',
-    'dsadadsdsad',
-    'dsadsadsada',
-    'sdaasda',
-    'dsadadsdsadsa',
+    'Das Nettoeinkommen bezieht sich auf Ihr Gehalt sowie alle Nebeneinkünfte nach Abzug der Steuern. Geben Sie den Betrag an, der Ihnen monatlich tatsächlich ausgezahlt wird.Berücksichtigen Sie auch regelmäßige zusätzliche Einnahmen wie Boni oder Provisionen.Falls Sie den exakten Wert nicht kennen, schätzen Sie bitte den monatlichen Nettobetrag ',
+    'Die Mietkosten umfassen den monatlichen Betrag, den Sie für Ihre Wohnung oder Ihr Haus zahlen. Geben Sie ausschließlich die reine Miete an, ohne Nebenkosten, sofern diese separat erfasst werden. Falls in Ihrer Miete bereits Nebenkosten enthalten sind, sollten Sie dies in Ihrer Kalkulation berücksichtigen. Falls Unsicherheiten bestehen, schätzen Sie den monatlichen Mietpreis.',
+    'Die Nebenkosten beinhalten Ausgaben für Strom, Wasser, Heizung, Müllabfuhr und andere laufende Kosten.\n' +
+      'Geben Sie den durchschnittlichen Betrag an, den Sie monatlich hierfür aufwenden.\n' +
+      'Berücksichtigen Sie, dass diese Kosten je nach Verbrauch variieren können.\n' +
+      'Falls Sie den exakten Betrag nicht kennen, geben Sie bitte einen realistischen Schätzwert an.',
+    'Hier sollten Sie alle Ausgaben für Lebensmittel, Getränke und Restaurantbesuche eintragen.\n' +
+      'Geben Sie den monatlichen Durchschnittswert an, den Sie für Ihre Ernährung ausgeben.\n' +
+      'Denken Sie daran, sowohl Supermarkteinkäufe als auch gelegentliche Restaurantbesuche einzubeziehen.\n' +
+      'Falls Sie den genauen Betrag nicht kennen, schätzen Sie ihn bitte basierend auf Ihren bisherigen Ausgaben.\n',
+    'Die Mobilitätskosten umfassen alle Ausgaben im Zusammenhang mit Ihrem Transport, wie Benzin, Tickets für öffentliche Verkehrsmittel oder Wartungskosten.\n' +
+      'Geben Sie den monatlichen Betrag an, den Sie für Ihre Mobilität aufwenden.\n' +
+      'Falls Sie mehrere Verkehrsmittel nutzen, addieren Sie bitte die entsprechenden Kosten.\n' +
+      'Schätzen Sie den Betrag, falls er monatlich variiert.',
+    'Unter Hobbies und Freizeit fallen alle Ausgaben, die Sie für Ihre persönlichen Interessen und Aktivitäten tätigen.\n' +
+      'Dazu gehören Kosten für Sport, Kultur, Freizeitaktivitäten oder auch Mitgliedschaften in Vereinen.\n' +
+      'Geben Sie den durchschnittlichen monatlichen Betrag an, den Sie hierfür ausgeben.\n' +
+      'Falls Sie nicht den exakten Betrag wissen, schätzen Sie bitte den üblichen Monatswert.',
+    'Versicherungen umfassen regelmäßige Beiträge für Kranken-, Auto-, Hausrat- und andere Versicherungen.\n' +
+      'Geben Sie den monatlichen Durchschnittsbetrag an, den Sie für all Ihre Versicherungen zahlen.\n' +
+      'Falls einige Versicherungen nur jährlich oder vierteljährlich gezahlt werden, berechnen Sie einen entsprechenden Monatsdurchschnitt.\n' +
+      'Schätzen Sie den Betrag, wenn Sie nicht genau wissen, wie hoch Ihre monatlichen Ausgaben sind.',
+    'Beim Sparen handelt es sich um den Betrag, den Sie monatlich zur Seite legen oder investieren möchten.\n' +
+      'Geben Sie an, wie viel Geld Sie bewusst für Rücklagen oder Investitionen zurückstellen.\n' +
+      'Berücksichtigen Sie dabei alle Sparformen, sei es auf einem Sparkonto, in Fonds oder anderen Anlageformen.\n' +
+      'Falls Sie sich unsicher sind, wählen Sie bitte einen Betrag, der realistisch zu Ihren finanziellen Zielen passt.',
   ];
+
+  useEffect(() => {
+    const combinedCosts =
+      rent +
+      sideCosts +
+      foodAndDrinksCosts +
+      mobilityCosts +
+      hobbyCosts +
+      insuranceCosts +
+      monthlySavingsCosts;
+    setSaldo(income - combinedCosts);
+  }, [
+    rent,
+    sideCosts,
+    foodAndDrinksCosts,
+    mobilityCosts,
+    hobbyCosts,
+    insuranceCosts,
+    monthlySavingsCosts,
+    saldo,
+  ]);
 
   return (
     <div className="budgetform">
@@ -52,7 +91,7 @@ export default function BudgetForm() {
           onChange={(e) => {
             // Regex to remove non-numeric characters and € symbol
             const rawValue = e.target.value.replace(/[^0-9]/g, '');
-            setIncome(rawValue);
+            setIncome(Number(rawValue));
           }}
         />
         <div className="tooltip-container">
@@ -67,11 +106,11 @@ export default function BudgetForm() {
           onChange={(e) => {
             // Regex to remove non-numeric characters and € symbol
             const rawValue = e.target.value.replace(/[^0-9]/g, '');
-            setRent(rawValue);
+            setRent(Number(rawValue));
           }}
         />
         <div className="tooltip-container">
-          <Icon name="Question" text={textArr[2]} />
+          <Icon name="Question" text={textArr[1]} />
         </div>
         <label>Nebenkosten *</label>
         <input
@@ -81,11 +120,11 @@ export default function BudgetForm() {
           onChange={(e) => {
             // Regex to remove non-numeric characters and € symbol
             const rawValue = e.target.value.replace(/[^0-9]/g, '');
-            setSideCosts(rawValue);
+            setSideCosts(Number(rawValue));
           }}
         />
         <div className="tooltip-container">
-          <Icon name="Question" text={textArr[3]} />
+          <Icon name="Question" text={textArr[2]} />
         </div>
         <label>Essen und Trinken *</label>
         <input
@@ -95,11 +134,11 @@ export default function BudgetForm() {
           onChange={(e) => {
             // Regex to remove non-numeric characters and € symbol
             const rawValue = e.target.value.replace(/[^0-9]/g, '');
-            setFoodAndDrinksCosts(rawValue);
+            setFoodAndDrinksCosts(Number(rawValue));
           }}
         />
         <div className="tooltip-container">
-          <Icon name="Question" text={textArr[4]} />
+          <Icon name="Question" text={textArr[3]} />
         </div>
         <label>Mobilität *</label>
         <input
@@ -109,12 +148,12 @@ export default function BudgetForm() {
           onChange={(e) => {
             // Regex to remove non-numeric characters and € symbol
             const rawValue = e.target.value.replace(/[^0-9]/g, '');
-            setMobilityCosts(rawValue);
+            setMobilityCosts(Number(rawValue));
           }}
         />
 
         <div className="tooltip-container">
-          <Icon name="Question" text={textArr[5]} />
+          <Icon name="Question" text={textArr[4]} />
         </div>
         <label>Hobbies und Freizeit *</label>
         <input
@@ -124,11 +163,11 @@ export default function BudgetForm() {
           onChange={(e) => {
             // Regex to remove non-numeric characters and € symbol
             const rawValue = e.target.value.replace(/[^0-9]/g, '');
-            setHobbyCosts(rawValue);
+            setHobbyCosts(Number(rawValue));
           }}
         />
         <div className="tooltip-container">
-          <Icon name="Question" text={textArr[6]} />
+          <Icon name="Question" text={textArr[5]} />
         </div>
         <label>Versicherungen *</label>
         <input
@@ -138,11 +177,11 @@ export default function BudgetForm() {
           onChange={(e) => {
             // Regex to remove non-numeric characters and € symbol
             const rawValue = e.target.value.replace(/[^0-9]/g, '');
-            setInsuranceCosts(rawValue);
+            setInsuranceCosts(Number(rawValue));
           }}
         />
         <div className="tooltip-container">
-          <Icon name="Question" text={textArr[7]} />
+          <Icon name="Question" text={textArr[6]} />
         </div>
         <label>Sparen </label>
         <input
@@ -152,15 +191,15 @@ export default function BudgetForm() {
           onChange={(e) => {
             // Regex to remove non-numeric characters and € symbol
             const rawValue = e.target.value.replace(/[^0-9]/g, '');
-            setMonthlySavingsCosts(rawValue);
+            setMonthlySavingsCosts(Number(rawValue));
           }}
         />
         <div className="tooltip-container">
-          <Icon name="Question" text={textArr[8]} />
+          <Icon name="Question" text={textArr[7]} />
         </div>
       </form>
       <p>* Pflichtfelder</p>
-      <p>Saldo: </p>
+      <p>Saldo: {saldo}€</p>
       <Button type="submit">Speichern</Button>
     </div>
   );
